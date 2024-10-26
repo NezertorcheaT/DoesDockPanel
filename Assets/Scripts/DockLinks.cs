db3c3bbd4cb789535d0e16014c7a0b858c5cbf7b
NezertorcheaT <tigrisha06ps@gmail.com>
2024-10-26 14:03:52 +0300

    allmost nothin

--- Assets/Scripts/DockLinks.cs
+++ Assets/Scripts/DockLinks.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,15 +13,19 @@ public class DockLinks : MonoBehaviour, IEntriable
     {
     }
 
-    public abstract class FileObject
+    public abstract class FileObject : IDisposable
     {
         public Texture2D Image;
         public string File;
         public override string ToString() => $"{base.ToString()} {{ File: {File}, Image: {Image} }}";
+        public abstract void Dispose();
     }
 
     public class Link : FileObject
     {
+        public override void Dispose()
+        {
+        }
     }
 
     public class Folder : FileObject
@@ -36,36 +41,50 @@ public class DockLinks : MonoBehaviour, IEntriable
 
         public async Task UpdateLinks()
         {
-            Links.Clear();
             await Populate(Links, File);
+        }
+
+        public override void Dispose()
+        {
+            Links.Clear();
         }
     }
 
     public readonly ObservableList<FileObject> Links = new();
 
-    private async void UpdateImages()
+    public async void UpdateImages()
     {
-        Links.Clear();
         await Populate(Links, ConfigEntry.Instance.LinksPath);
     }
 
     private static async Task Populate(ICollection<FileObject> collection, string root)
     {
-        var textures = await Task.WhenAll(Directory
-            .EnumerateFileSystemEntries(root, "**", new EnumerationOptions { IgnoreInaccessible = true })
-            .Select(i => i
-                .Replace('/', Path.DirectorySeparatorChar)
-                .Replace('\\', Path.DirectorySeparatorChar)
-            )
-            .OrderBy(i => !Directory.Exists(i))
-            .ThenBy(i => i
-                .Split(Path.DirectorySeparatorChar)
-                .Last()
-            )
-            .Select(async i => (i, await FileThumbnail.GetThumbnail(i)))
-        );
-        foreach (var (file, texture) in textures)
+        for (var i = 0; i < collection.Count; i++)
         {
+            var fileObject = collection.First();
+            fileObject.Dispose();
+            collection.Remove(fileObject);
+        }
+
+        Debug.Log(collection.Count);
+        foreach (
+            var (file, texture)
+            in await Task.WhenAll(Directory
+                .EnumerateFileSystemEntries(root, "**", new EnumerationOptions { IgnoreInaccessible = true })
+                .Select(i => i
+                    .Replace('/', Path.DirectorySeparatorChar)
+                    .Replace('\\', Path.DirectorySeparatorChar)
+                )
+                .OrderBy(i => !Directory.Exists(i))
+                .ThenBy(i => i
+                    .Split(Path.DirectorySeparatorChar)
+                    .Last()
+                )
+                .Select(async i => (i, await FileThumbnail.GetThumbnail(i)))
+            )
+        )
+        {
+            Debug.Log(file);
             if (!Directory.Exists(file))
                 collection.Add(new Link { Image = texture, File = file });
             else
@@ -75,6 +94,8 @@ public class DockLinks : MonoBehaviour, IEntriable
                 collection.Add(folder);
             }
         }
+
+        Debug.Log(collection.Count);
     }
 
     void IEntriable.Begin()
