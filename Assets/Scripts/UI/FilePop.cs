b6fc2d5c9a2274eff4303b16ea5ec7882f14adc7
NezertorcheaT <tigrisha06ps@gmail.com>
2024-10-27 16:35:42 +0300

    better settings button

--- Assets/Scripts/UI/FilePop.cs
+++ Assets/Scripts/UI/FilePop.cs
@@ -11,19 +11,21 @@ namespace UI
 
         private void Start()
         {
-            fileUI.Click.Subscribe(_ => Tween.Scale(fileUI.transform,
-                new TweenSettings<Vector3>(new Vector3(1, 1, 1), 0.4f, Ease.OutElastic)));
+            fileUI.Click.Subscribe(_ =>
+                Tween
+                    .Scale(fileUI.transform, new TweenSettings<float>(1.2f, 0.1f, Ease.OutElastic))
+                    .Chain(Tween.Scale(fileUI.transform, new TweenSettings<float>(1, 0.1f, Ease.InCubic)))
+            );
         }
 
         public void OnPointerEnter(PointerEventData eventData)
         {
-            Tween.Scale(fileUI.transform,
-                new TweenSettings<Vector3>(new Vector3(1.1f, 1.1f, 1.1f), 0.4f, Ease.OutCubic));
+            Tween.Scale(fileUI.transform, new TweenSettings<float>(1.1f, 0.2f, Ease.OutCubic));
         }
 
         public void OnPointerExit(PointerEventData eventData)
         {
-            Tween.Scale(fileUI.transform, new TweenSettings<Vector3>(new Vector3(1, 1, 1), 0.4f, Ease.OutCubic));
+            Tween.Scale(fileUI.transform, new TweenSettings<float>(1, 0.2f, Ease.OutCubic));
         }
     }
 }
--- Assets/Scripts/UI/SettingsUI.cs
+++ Assets/Scripts/UI/SettingsUI.cs
@@ -8,48 +8,72 @@ using UnityEngine.EventSystems;
 
 namespace UI
 {
-    public class SettingsUI : MonoBehaviour, IPointerEnterHandler
+    public class SettingsUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     {
         [SerializeField] private Button closeButton;
+        [SerializeField] private RectTransform openButton;
         [SerializeField] private Image trigger;
         [SerializeField] private RectTransform container;
-        public Observable<Unit> OnEntered { get; private set; }
-
         private Subject<Unit> _onEntered;
+        private Subject<Unit> _onExited;
         private bool _opened;
+        private bool _hovered;
         private IDisposable _disposableEvents;
 
         private void OnEnable()
         {
             _onEntered = new Subject<Unit>();
+            _onExited = new Subject<Unit>();
             var disposer = Disposable.CreateBuilder();
 
-            OnEntered = _onEntered
-                    .Where(_ => !_opened)
-                    .Do(_ => _opened = true)
+            _onEntered
+                .Where(_ => !_hovered && !_opened)
+                .Do(_ => _hovered = true)
+                .Subscribe(_ => Tween.Scale(openButton, 1, 0.2f, Ease.OutCubic))
+                .AddTo(ref disposer)
                 ;
-            OnEntered.Subscribe(_ => Open()).AddTo(ref disposer);
+            _onExited
+                .Where(_ => _hovered && !_opened)
+                .Do(_ => _hovered = false)
+                .Subscribe(_ => Tween.Scale(openButton, 0, 0.2f, Ease.OutCubic))
+                .AddTo(ref disposer)
+                ;
 
-            closeButton.onClick.AsObservable()
-                .Where(_ => _opened)
-                .Do(_ => _opened = false)
-                .Subscribe(_ => Close()).AddTo(ref disposer);
+            closeButton.onClick.AsObservable().Subscribe(_ => Close()).AddTo(ref disposer);
 
             _disposableEvents = disposer.Build();
+            openButton.localScale = Vector3.zero;
         }
 
         private void Start()
         {
-            Close();
+            CloseAnim();
         }
 
-        private void Open()
+        public void Open()
+        {
+            if (!_opened)
+                OpenAnim();
+            Tween.Scale(openButton, 0, 0.2f, Ease.OutCubic);
+            _opened = true;
+            _hovered = false;
+        }
+
+        public void Close()
+        {
+            if (_opened)
+                CloseAnim();
+            _opened = false;
+            _hovered = false;
+        }
+
+        private void OpenAnim()
         {
             Tween.UIAnchoredPosition(container,
                 new TweenSettings<Vector2>(ConfigEntry.Instance.SettingsPosition, 0.4f, Ease.OutCubic));
         }
 
-        private void Close()
+        private void CloseAnim()
         {
             ConfigEntry.Instance.SettingsPosition = container.anchoredPosition;
             Tween.UIAnchoredPosition(container,
@@ -65,8 +89,14 @@ namespace UI
 
         public void OnPointerEnter(PointerEventData eventData)
         {
-            if (eventData.hovered.Contains(trigger.gameObject))
+            if (eventData.hovered.Contains(trigger.gameObject) || eventData.hovered.Contains(openButton.gameObject))
                 _onEntered.OnNext(new Unit());
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            if (eventData.hovered.Contains(trigger.gameObject) || eventData.hovered.Contains(openButton.gameObject))
+                _onExited.OnNext(new Unit());
         }
     }
 }
