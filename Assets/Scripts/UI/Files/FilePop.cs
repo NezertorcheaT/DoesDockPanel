4089bd1a7512c1655518a66041bdc5b57f6bd423
NezertorcheaT <tigrisha06ps@gmail.com>
2025-01-09 18:54:43 +0300

    FilePop imageContainer

--- Assets/Scripts/UI/Files/FilePop.cs
+++ Assets/Scripts/UI/Files/FilePop.cs
@@ -7,25 +7,26 @@ namespace UI.Files
 {
     public class FilePop : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     {
+        [SerializeField] private Transform imageContainer;
         [SerializeField] private FileUI fileUI;
 
         private void Start()
         {
             fileUI.LeftClick.Subscribe(_ =>
                 Tween
-                    .Scale(fileUI.transform, new TweenSettings<float>(1.2f, 0.1f, Ease.OutElastic))
-                    .Chain(Tween.Scale(fileUI.transform, new TweenSettings<float>(1, 0.1f, Ease.InCubic)))
+                    .Scale(imageContainer.transform, new TweenSettings<float>(1.2f, 0.1f, Ease.OutElastic))
+                    .Chain(Tween.Scale(imageContainer.transform, new TweenSettings<float>(1, 0.1f, Ease.InCubic)))
             );
         }
 
         public void OnPointerEnter(PointerEventData eventData)
         {
-            Tween.Scale(fileUI.transform, new TweenSettings<float>(1.1f, 0.2f, Ease.OutCubic));
+            Tween.Scale(imageContainer.transform, new TweenSettings<float>(1.1f, 0.2f, Ease.OutCubic));
         }
 
         public void OnPointerExit(PointerEventData eventData)
         {
-            Tween.Scale(fileUI.transform, new TweenSettings<float>(1, 0.2f, Ease.OutCubic));
+            Tween.Scale(imageContainer.transform, new TweenSettings<float>(1, 0.2f, Ease.OutCubic));
         }
     }
 }
